a3ab55754698aaa56912023602d7e88fe4e82909
Abhishek N N <7abhisheknn@gmail.com>
2023-07-12 09:13:09 +0530

    squares done

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <p>
+      {{value}}
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SquareComponent {
+  @Input() value!: 'X' | 'O';
+}
